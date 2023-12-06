860015f972b49fd14d481085fb54a39b30213951
Marlus Marcos <marlusufrn@gmail.com>
2023-12-06 16:01:56 -0300

    Volume done

--- src/main/java/com/thealgorithms/maths/Area.java
+++ src/main/java/com/thealgorithms/maths/Area.java
@@ -25,19 +25,23 @@ public class Area {
     //@ spec_public
     private static final /*@ non_null @*/ String POSITIVE_BASE = "Must be a positive base";
 
-    // @ ensures \result > 0;
-    // @ ensures \result == 6 * sideLength * sideLength;
+   
     //@ requires sideLength > 0;
-    public static /*@ pure @*/ double surfaceAreaCube(final double sideLength) {
+    //@ ensures \result == 6 * sideLength * sideLength;
+    //@ exceptional_behavior requires (sideLength <= 0);
+    //@ signals (IllegalArgumentException) sideLength <= 0;
+    public static /*@ pure*/  double surfaceAreaCube(final double sideLength) {
         if (sideLength <= 0) {
             throw new IllegalArgumentException("Must be a positive sideLength");
         }
         return 6 * sideLength * sideLength;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == 4 * Math.PI * radius * radius;
+    
     //@ requires radius > 0;
+    //@ ensures \result == 4 * Math.PI * radius * radius;
+    //@ exceptional_behavior requires (radius <= 0);
+    //@ signals (IllegalArgumentException) radius <= 0;
     public static /*@ pure @*/ double surfaceAreaSphere(final double radius) {
         if (radius <= 0) {
             throw new IllegalArgumentException(POSITIVE_RADIUS);
@@ -45,9 +49,13 @@ public class Area {
         return 4 * Math.PI * radius * radius;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == length * width;
+
     //@ requires length > 0 && width > 0;
+    //@ ensures \result == length * width;
+    //@ exceptional_behavior requires (length <= 0);
+    //@ signals (IllegalArgumentException) length <= 0;
+    //@ exceptional_behavior requires (width <= 0);
+    //@ signals (IllegalArgumentException) width <= 0;
     public static /*@ pure @*/ double surfaceAreaRectangle(final double length, final double width) {
         if (length <= 0) {
             throw new IllegalArgumentException("Must be a positive length");
@@ -58,9 +66,13 @@ public class Area {
         return length * width;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == 2 * (Math.PI * radius * radius + Math.PI * radius * height);
+   
     //@ requires radius > 0 && height > 0;
+    //@ ensures \result == 2 * (Math.PI * radius * radius + Math.PI * radius * height);
+    //@ exceptional_behavior requires (radius <= 0);
+    //@ signals (IllegalArgumentException) radius <= 0;
+    //@ exceptional_behavior requires (height <= 0);
+    //@ signals (IllegalArgumentException) height <= 0;
     public static /*@ pure @*/ double surfaceAreaCylinder(final double radius, final double height) {
         if (radius <= 0) {
             throw new IllegalArgumentException(POSITIVE_RADIUS);
@@ -71,9 +83,12 @@ public class Area {
         return 2 * (Math.PI * radius * radius + Math.PI * radius * height);
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == sideLength * sideLength;
+ 
+    
     //@ requires sideLength > 0;
+    //@ ensures \result == sideLength * sideLength;
+    //@ exceptional_behavior requires (sideLength <= 0);
+    //@ signals (IllegalArgumentException) sideLength <= 0;
     public static /*@ pure @*/ double surfaceAreaSquare(final double sideLength) {
         if (sideLength <= 0) {
             throw new IllegalArgumentException("Must be a positive sideLength");
@@ -81,9 +96,14 @@ public class Area {
         return sideLength * sideLength;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == base * height / 2;
+
+
     //@ requires base > 0 && height > 0;
+    //@ ensures \result == base * height / 2;
+    //@ exceptional_behavior requires (base <= 0);
+    //@ signals (IllegalArgumentException) base <= 0;
+    //@ exceptional_behavior requires (height <= 0);
+    //@ signals (IllegalArgumentException) height <= 0;
     public static /*@ pure @*/ double surfaceAreaTriangle(final double base, final double height) {
         if (base <= 0) {
             throw new IllegalArgumentException(POSITIVE_BASE);
@@ -94,9 +114,14 @@ public class Area {
         return base * height / 2;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == base * height;
+   
+    
     //@ requires base > 0 && height > 0;
+    //@ ensures \result == base * height;
+    //@ exceptional_behavior requires (base <= 0);
+    //@ signals (IllegalArgumentException) base <= 0;
+    //@ exceptional_behavior requires (height <= 0);
+    //@ signals (IllegalArgumentException) height <= 0;
     public static /*@ pure @*/ double surfaceAreaParallelogram(final double base, final double height) {
         if (base <= 0) {
             throw new IllegalArgumentException(POSITIVE_BASE);
@@ -107,9 +132,16 @@ public class Area {
         return base * height;
     }
 
-    /* @ ensures \result > 0; */
-    // @ ensures \result == (base1 + base2) * height / 2;
+ 
+    
     //@ requires base1 > 0 && base2 > 0 && height > 0;
+    //@ ensures \result == (base1 + base2) * height / 2;
+    // @ exceptional_behavior requires (base1 <= 0);
+    // @ signals (IllegalArgumentException) base1 <= 0;
+    // @ exceptional_behavior requires (base2 <= 0);
+    // @ signals (IllegalArgumentException) base2 <= 0;
+    // @ exceptional_behavior requires (height <= 0);
+    // @ signals (IllegalArgumentException) height <= 0;
     public static /*@ pure @*/ double surfaceAreaTrapezium(final double base1, final double base2, final double height) {
         if (base1 <= 0) {
             throw new IllegalArgumentException(POSITIVE_BASE + 1);
@@ -124,33 +156,38 @@ public class Area {
     }
 
     //@ requires radius > 0;
-    // @ ensures \result > 0;
-    // @ ensures \result == Math.PI * radius * radius;
+    //@ ensures \result == Math.PI * radius * radius;
+    //@ exceptional_behavior requires (radius <= 0);
+    //@ signals (IllegalArgumentException) radius <= 0;
     public static /*@ pure @*/ double surfaceAreaCircle(final double radius) {
         if (radius <= 0) {
             throw new IllegalArgumentException(POSITIVE_RADIUS);
         }
         return Math.PI * radius * radius;
     }
-
-    // @ ensures \result > 0; 
-    // @ ensures \result == 3 * Math.PI * radius * radius;
     //@ requires radius > 0;
-    public static /*@ pure @*/ double surfaceAreaHemisphere(final double radius) {
+    //@ ensures \result == 3.0 * Math.PI * radius * radius;
+    //@ exceptional_behavior requires (radius <= 0);
+    //@ signals (IllegalArgumentException) radius <= 0;
+    public static double surfaceAreaHemisphere(final double radius) {
         if (radius <= 0) {
             throw new IllegalArgumentException(POSITIVE_RADIUS);
         }
-        return 3 * Math.PI * radius * radius;
+        return 3.0 * Math.PI * radius * radius;
     }
 
-    //@ requires Double.isNaN(radius);
-    //@ requires Double.isNaN(height);
-    //@ ensures Double.isNaN(\result);
+    //@ requires !Double.isNaN(radius);
+    //@ requires !Double.isNaN(height);
+    //@ ensures !Double.isNaN(\result);
     //@ requires height * height + radius == 0.0;
     //@ ensures \result == 1.0;
-    //@ requires radius > 0.0 && height > 0.0;
+    //@ requires radius > 0.0 && height > 0.0 && Math.PI > 0.0;
     //@ ensures \result > 0;
     //@ ensures \result == Math.PI * radius * (radius + Math.pow(height * height + radius * radius, 0.5));
+    //@ exceptional_behavior requires (radius <= 0);
+    //@ signals (IllegalArgumentException) radius <= 0;
+    //@ exceptional_behavior requires (height <= 0);
+    //@ signals (IllegalArgumentException) height <= 0;
     public static double surfaceAreaCone(final double radius, final double height) {
         if (radius <= 0) {
             throw new IllegalArgumentException(POSITIVE_RADIUS);
