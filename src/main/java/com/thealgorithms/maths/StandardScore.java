4a7e96871e2b0141a7fe9a90eb09bb69305c3952
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-12-07 23:15:47 -0300

    Adicionando anotações em StandarScore.java.

--- src/main/java/com/thealgorithms/maths/StandardScore.java
+++ src/main/java/com/thealgorithms/maths/StandardScore.java
@@ -1,7 +1,12 @@
 package com.thealgorithms.maths;
 
 public class StandardScore {
-
+	
+	//@ requires num > 0;
+	//@ requires mean > 0;
+	//@ requires stdDev > 0;
+    //@ ensures \result == (num - mean) / stdDev;
+	//@ signals (ArithmeticException) stdDev == 0; 
     public static double zScore(double num, double mean, double stdDev) {
         return (num - mean) / stdDev;
     }
