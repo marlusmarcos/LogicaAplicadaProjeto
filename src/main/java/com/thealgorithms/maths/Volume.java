3fafbc4cca19fce05873f76f0c3ac501c267458f
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-12-07 22:56:57 -0300

    Adicionando anotações em Volume.java.

--- src/main/java/com/thealgorithms/maths/Volume.java
+++ src/main/java/com/thealgorithms/maths/Volume.java
@@ -2,43 +2,72 @@ package com.thealgorithms.maths;
 
 /* Calculate the volume of various shapes.*/
 public class Volume {
-
- 
-    public static double volumeCube(double sideLength) {
+	
+	//@ requires sideLength > 0;
+	//@ ensures \result == sideLength * sideLength * sideLength;
+	//@ signals (IllegalArgumentException) sideLength <= 0; 
+    public static /*@ pure @*/ double volumeCube(double sideLength) {
         return sideLength * sideLength * sideLength;
     }
 
-
-    public static double volumeCuboid(double width, double height, double length) {
+	//@ requires width > 0;
+	//@ requires height > 0;
+	//@ requires length > 0;
+	//@ ensures \result == width * height * length;
+	//@ signals (IllegalArgumentException) width <= 0;
+	//@ signals (IllegalArgumentException) height <= 0;
+	//@ signals (IllegalArgumentException) length <= 0;
+    public static /*@ pure @*/ double volumeCuboid(double width, double height, double length) {
         return width * height * length;
     }
 
-
-    public static double volumeSphere(double radius) {
+    //@ requires radius > 0;
+    //@ ensures \result == (4 * Math.PI * radius * radius * radius) / 3;
+    //@ signals (IllegalArgumentException) radius <= 0;
+    public static /*@ pure @*/ double volumeSphere(double radius) {
         return (4 * Math.PI * radius * radius * radius) / 3;
     }
-
- 
-    public static double volumeCylinder(double radius, double height) {
+    
+    //@ requires radius > 0;
+    //@ requires height > 0;
+    //@ ensures \result == Math.PI * radius * radius * height;
+    //@ signals (IllegalArgumentException) radius <= 0; 
+    //@ signals (IllegalArgumentException) height <= 0; 
+    public static /*@ pure @*/ double volumeCylinder(double radius, double height) {
         return Math.PI * radius * radius * height;
     }
-
-    public static double volumeHemisphere(double radius) {
+    
+    //@ requires radius > 0;
+    //@ ensures \result == (2 * Math.PI * radius * radius * radius) / 3;
+    //@ signals (IllegalArgumentException) radius <= 0; 
+    public static /*@ pure @*/ double volumeHemisphere(double radius) {
         return (2 * Math.PI * radius * radius * radius) / 3;
     }
-
- 
-    public static double volumeCone(double radius, double height) {
+    
+    //@ requires radius > 0;
+    //@ requires height > 0;
+    //@ ensures \result == (Math.PI * radius * radius * height) / 3;
+    //@ signals (IllegalArgumentException) radius <= 0;  
+    //@ signals (IllegalArgumentException) height <= 0; 
+    public static /*@ pure @*/ double volumeCone(double radius, double height) {
         return (Math.PI * radius * radius * height) / 3;
     }
 
-
-    public static double volumePrism(double baseArea, double height) {
+    //@ requires baseArea > 0;
+    //@ requires height > 0;
+    //@ ensures \result == baseArea * height;
+    //@ signals (IllegalArgumentException) baseArea <= 0;  
+    //@ signals (IllegalArgumentException) height <= 0; 
+    public static /*@ pure @*/ double volumePrism(double baseArea, double height) {
         return baseArea * height;
     }
 
-  
-    public static double volumePyramid(double baseArea, double height) {
+    //@ requires baseArea > 0;
+    //@ requires height > 0;
+    //@ ensures \result == (baseArea * height) / 3;
+    //@ signals (IllegalArgumentException) baseArea <= 0;  
+    //@ signals (IllegalArgumentException) height <= 0; 
+    public static /*@ pure @*/ double volumePyramid(double baseArea, double height) {
         return (baseArea * height) / 3;
     }
 }
