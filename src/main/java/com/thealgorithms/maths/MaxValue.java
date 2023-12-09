2c5de950425c34ab1895afc374f612146c57d78a
marlusmarcos <marlusufrn@gmail.com>
2023-12-09 09:12:19 -0300

    add valores de limites

--- src/main/java/com/thealgorithms/maths/MaxValue.java
+++ src/main/java/com/thealgorithms/maths/MaxValue.java
@@ -12,12 +12,13 @@ public final class MaxValue {
      * @param b another argument.
      * @return the larger of {@code a} and {@code b}.
      */
+    
     //@ requires true; 
     //@ requires Integer.MIN_VALUE < a < Integer.MAX_VALUE;
     //@ requires Integer.MIN_VALUE < b < Integer.MAX_VALUE;
     //@ ensures \result == a || \result == b; 
     //@ ensures (\result == a && a >= b) || (\result == b && b > a);
-    public static int max(int a, int b) {
+    public static /*@ pure*/  int max(int a, int b) {
         return a >= b ? a : b;
     }
 }
