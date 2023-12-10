5197210834273a33efb3db3abb0a6a4f6340df66
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-12-10 15:49:31 -0300

    Adicionando classe Perimetro.java.

--- src/main/java/com/thealgorithms/maths/Perimeter.java
+++ src/main/java/com/thealgorithms/maths/Perimeter.java
@@ -12,7 +12,10 @@ public class Perimeter {
      * @param side for length of each side.
      * @return Perimeter of given polygon
      */
-    public static float perimeterRegularPolygon(int n, float side) {
+	//@ requires n > 2;
+	//@ requires side >= 0; 
+    //@ ensures \result == n * side; 
+    public static /*@ pure @*/ float perimeterRegularPolygon(int n, float side) {
         return n * side;
     }
 
@@ -27,12 +30,18 @@ public class Perimeter {
      * @param sides for length of remaining sides
      * @return Perimeter of given trapezoid.
      */
-    public static float perimeterIrregularPolygon(float side1, float side2, float side3, float... sides) {
+    //@ requires side1 >= 0 && side2 >= 0 && side3 >= 0;
+    //@ ensures \result >= side1 + side2 + side3;    
+    public static /*@ pure @*/ float perimeterIrregularPolygon(float side1, float side2, float side3, float... sides) {
         float perimeter = side1 + side2 + side3;
         for (float side : sides) {
             perimeter += side;
         }
-        return perimeter;
+        if (perimeter >= side1 + side2 + side3) {
+            return perimeter;
+        } else {
+            throw new IllegalStateException("Invalid perimeter!");
+        }
     }
 
     /**
@@ -42,7 +51,10 @@ public class Perimeter {
      * @param breadth for breadth of rectangle
      * @return Perimeter of given rectangle
      */
-    public static float perimeterRectangle(float length, float breadth) {
+    //@ requires length >= 0; 
+    //@ requires breadth >= 0; 
+    //@ ensures \result == 2 * (length + breadth);
+    public static /*@ pure @*/ float perimeterRectangle(float length, float breadth) {
         return 2 * (length + breadth);
     }
 
@@ -52,7 +64,9 @@ public class Perimeter {
      * @param r for radius of circle.
      * @return circumference of given circle.
      */
-    public static double perimeterCircle(float r) {
+    //@ requires r >= 0; 
+    //@ ensures \result == 2 * Math.PI * r;
+    public static /*@ pure @*/ double perimeterCircle(float r) {
         return 2 * Math.PI * r;
     }
 }
